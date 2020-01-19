e5cbb945e4ecef7a266fb022b2bfc04fe9f0e71d
liudong <liudong.edward@gmail.com>
2020-01-19 17:40:58 +0800

    add framework version 1.4.1

---
+++ frameworks/libsrt.framework/Headers/libsrt.h
@@ -0,0 +1,18 @@
+//
+//  libsrt.h
+//  libsrt
+//
+//  Created by 刘栋 on 2020/1/19.
+//
+
+#ifndef libsrt_h
+#define libsrt_h
+
+#include "logging_api.h"
+#include "platform_sys.h"
+#include "srt.h"
+#include "srt4udt.h"
+#include "udt.h"
+#include "version.h"
+
+#endif /* libsrt_h */
